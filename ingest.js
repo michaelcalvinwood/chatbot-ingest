d843e5bcade38fb20f529e42ae8a8c6665a594a8
Michael Wood <michaelwood33311@icloud.com>
2023-04-28 20:01:00 -0400

    added generating presigned put url

--- ingest.js
+++ ingest.js
@@ -13,6 +13,7 @@ const fs = require('fs');
 
 const formidable = require('formidable');
 const jwt = require('./utils/jwt');
+const s3 = require('./utils/s3');
 
 const app = express();
 app.use(express.static('public'));
@@ -34,28 +35,34 @@ const ingestPdf = async (fileName, origName, token) => {
     return;
 }
 
+const handleSuppliedToken = (bt, res) => {
+    console.log('bt', bt);
+    const tokenInfo = jwt.extractToken(bt, true);
+    if (!tokenInfo.status) {
+        res.status(401).json('unauthorized');
+        resolve('error 401 unauthorized');
+        return false;
+    }
+
+    const token = tokenInfo.msg;
+
+    console.log('token', token);
+
+    if (token.serverSeries !== serverSeries) {
+        res.status(400).json('bad request');
+        resolve('error 400 bad request');
+        return false;
+    }
+
+    return token
+}
+
 const fileUpload = (req, res) => {
     return new Promise(async (resolve, reject) => {
         const { bt } = req.query;
-
-        console.log('bt', bt);
-        const tokenInfo = jwt.extractToken(bt, true);
-        if (!tokenInfo.status) {
-            res.status(401).json('unauthorized');
-            resolve('error 401 unauthorized');
-            return;
-        }
-
-        const token = tokenInfo.msg;
-
-        console.log('token', token);
-
-        if (token.serverSeries !== serverSeries) {
-            res.status(400).json('bad request');
-            resolve('error 400 bad request');
-            return;
-        }
-
+        const token = handleSuppliedToken(bt, res);
+        if (!token) return resolve('error bad token');
+        
         var form = new formidable.IncomingForm();
         form.parse(req, async function (err, fields, data) {
             //console.log('form data', data);
@@ -88,11 +95,24 @@ const fileUpload = (req, res) => {
     })
 }
 
-const presignedPostUrl = (req, res) => {
-    return new Promise((resolve, reject) => {
+const presignedUrl = (req, res) => {
+    return new Promise(async (resolve, reject) => {
+        const { bt } = req.query;
+        const token = handleSuppliedToken(bt, res);
+        if (!token) return resolve('error bad token');
+        const { fileName } = req.body;
+        if (!fileName) {
+            res.status(400).json('bad request');
+            return resolve('error bad request');
+        }
+
         let url;
+        const {S3_ENDPOINT, S3_ENDPOINT_DOMAIN, S3_REGION, S3_KEY, S3_SECRET, S3_BUCKET} = process.env;
+        console.log('s3', s3);
 
-
+        const s3Client = s3.client(S3_ENDPOINT, S3_ENDPOINT_DOMAIN, S3_REGION, S3_KEY, S3_SECRET, S3_BUCKET);
+        url = await s3.presignedUploadUrl(s3Client, token.botId);
+        
         res.status(200).json(url);
         resolve('ok');
     })
@@ -107,8 +127,8 @@ const ingestS3Object = (req, res) => {
 }
 
 app.post('/fileUpload', (req, res) => fileUpload(req, res));
-app.post('/presignedPostUrl', (req, res) => presignedPostUrl);
-app.post('/ingestS3Object', (req, res) => ingestS3Object);
+app.post('/presignedUrl', (req, res) => presignedUrl(req, res));
+app.post('/ingestS3Object', (req, res) => ingestS3Object(req, res));
 
 const httpsServer = https.createServer({
     key: fs.readFileSync(privateKeyPath),
