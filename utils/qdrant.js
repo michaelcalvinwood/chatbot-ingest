8413024b0952ffa18f908fb36e12e17b15572be0
Michael Wood <michaelwood33311@icloud.com>
2023-04-30 12:58:59 -0400

    added util to create qdrant collection

---
+++ utils/qdrant.js
@@ -0,0 +1,29 @@
+require ('dotenv').config();
+const axios = require('axios');
+const { Configuration, OpenAIApi } = require("openai");
+const { v4: uuidv4 } = require('uuid');
+
+exports.createCollection = async (host, port, collectionName, size, onDiskPayload = false, distance = 'Cosine') => {
+    const request = {
+        url: `http://${host}:${port}/collections/${collectionName}`,
+        method: 'put',
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            "Access-Control-Allow-Origin": "*",
+        },
+        data: {
+            vectors: {
+                size,
+                distance
+            }
+        }
+    }
+
+    //console.log('onDiskPayload', onDiskPayload);
+
+    if (onDiskPayload) request.data.on_disk_payload = true;
+
+
+        
+    return promisfiedAxios(request);   
+}
