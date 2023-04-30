59ce334a859968d8ac986de35ef4f2277780cd3d
Michael Wood <michaelwood33311@icloud.com>
2023-04-30 09:16:40 -0400

    added extracting original filename from s3 url

--- ingest.js
+++ ingest.js
@@ -10,10 +10,13 @@ const express = require('express');
 const https = require('https');
 const cors = require('cors');
 const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const formidable = require('formidable');
 const jwt = require('./utils/jwt');
 const s3 = require('./utils/s3');
+const pdf = require('./utils/pdf');
 
 const app = express();
 app.use(express.static('public'));
@@ -99,7 +102,10 @@ const presignedUrl = (req, res) => {
     return new Promise(async (resolve, reject) => {
         const { bt } = req.query;
         const token = handleSuppliedToken(bt, res);
-        if (!token) return resolve('error bad token');
+        if (!token) {
+            res.status(400).json('bad request');
+            return resolve('error bad token');
+        }
         const { fileName } = req.body;
         if (!fileName) {
             res.status(400).json('bad request');
@@ -111,15 +117,58 @@ const presignedUrl = (req, res) => {
         console.log('s3', s3);
 
         const s3Client = s3.client(S3_ENDPOINT, S3_ENDPOINT_DOMAIN, S3_REGION, S3_KEY, S3_SECRET, S3_BUCKET);
-        url = await s3.presignedUploadUrl(s3Client, token.botId);
+        url = await s3.presignedUploadUrl(s3Client, token.botId + '/' + fileName);
         
         res.status(200).json(url);
         resolve('ok');
     })
 }
 
-const ingestS3Object = (req, res) => {
-    return new Promise((resolve, reject) => {
+const ingestS3Pdf = (req, res) => {
+    return new Promise(async (resolve, reject) => {
+        const { bt } = req.query;
+        const token = handleSuppliedToken(bt, res);
+        if (!token) {
+            res.status(400).json('bad request');
+            return resolve('error bad token');
+        }
+        
+        const { url } = req.body;
+        if (!url) {
+            res.status(400).json('bad request');
+            return resolve('error bad token');
+        }
+
+        const urlInfo = new URL(url);
+        console.log('urlInfo', urlInfo);
+        const fullFileName = path.basename(urlInfo.pathname);
+        const loc = fullFileName.indexOf('--');
+        const origFileName = fullFileName.substring(loc+2);
+        /*
+         * TODO: validate that url folder matches token.botId
+         */
+
+         /*
+         * TODO Prefetch filesize and see if the account has sufficient tokens
+         * if so, decrement tokens and process
+         * else 
+         *      send email alerting to the issue
+         *      delete the resource from S3 bucket
+         *      send error message
+         */
+
+        const fileName = `/home/tmp/${uuidv4()}.pdf`;
+        let result;
+
+        try {
+            result = await s3.download(url, fileName);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json('could not access uploaded file');
+            return resolve('error 500: could not access uploaded file');
+        }
+
+        ingestPdf(fileName, origFileName, token);
 
         res.status(200).json('ok');
         resolve('ok');
@@ -128,7 +177,7 @@ const ingestS3Object = (req, res) => {
 
 app.post('/fileUpload', (req, res) => fileUpload(req, res));
 app.post('/presignedUrl', (req, res) => presignedUrl(req, res));
-app.post('/ingestS3Object', (req, res) => ingestS3Object(req, res));
+app.post('/ingestS3Pdf', (req, res) => ingestS3Pdf(req, res));
 
 const httpsServer = https.createServer({
     key: fs.readFileSync(privateKeyPath),
---
+++ utils/pdf.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const fsPromise = require('fs').promises;
+const pdfExtraction = require("pdf-extraction");
+
+const deleteFile = fileName => {
+    return fs.unlink(fileName, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("Delete File successfully.");
+    });
+}
+
+exports.extractPdf = (fileName, del = false) => {
+    return new Promise(async (resolve, reject) => {
+        let data;
+
+        try {
+            const dataBuffer = await fsPromise.readFile(fileName);
+            data = await pdfExtraction(dataBuffer);
+        } catch (err) {
+            console.error(err);
+            if (del) deleteFile(fileName)
+            reject(err);
+        }
+        
+        console.log(data);
+        if (del) deleteFile(fileName);
+        return resolve(data);
+    })
+}
